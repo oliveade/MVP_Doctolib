43df4df1570a12f3edec8adb963a7edc404145a7
oliveade <olive.ade@eemi.com>
2025-09-25 12:08:43 +0200

    acceuil

--- compagnon-sante/src/components/home/AppointmentAlert.tsx
+++ compagnon-sante/src/components/home/AppointmentAlert.tsx
@@ -4,12 +4,12 @@ export default function AppointmentAlert({ dateLabel }: { dateLabel: string }) {
   return (
     <div className="rounded-[16px] bg-white shadow-[0_8px_18px_rgba(0,0,0,0.08)] p-4">
       <div className="flex items-start gap-3">
-        <div className="h-12 w-12 rounded-2xl bg-[#FFF3C9] flex items-center justify-center">
+        <div className="h-12 w-12 rounded-2xl flex items-center justify-center">
           <Image src="/avatar/doctor.png" alt="" width={45} height={45}/>
         </div>
         <div className="flex-1">
-          <div className="font-semibold text-[16px]">Votre rendez-vous est dans 2 jours !</div>
-          <div className="text-[14px] text-slate-600 mt-2">{dateLabel}</div>
+          <div className="font-semibold text-[13px]">Votre rendez-vous est dans 2 jours !</div>
+          <div className="text-[12px] text-slate-600 mt-2">{dateLabel}</div>
         </div>
       </div>
     </div>
--- compagnon-sante/src/components/home/ProgressCard.tsx
+++ compagnon-sante/src/components/home/ProgressCard.tsx
@@ -19,12 +19,12 @@ export default function ProgressCard({
     return (
         <div className="rounded-[16px] bg-white shadow-[0_8px_18px_rgba(0,0,0,0.08)] p-4">
             <div className="flex items-start gap-3">
-                <div className="h-12 w-12 rounded-2xl bg-[#FFF3C9] flex items-center justify-center">
+                <div className="h-12 w-12 rounded-2xl flex items-center justify-center">
                     <Image src={icon} alt="" width={45} height={45} />
                 </div>
                 <div className="flex-1">
                     <div className="flex text-center justify-between">
-                        <h3 className="font-semibold text-[17px] text-slate-900">{title}</h3>
+                        <h3 className="font-semibold text-[14px] text-slate-900">{title}</h3>
                         <span className="text-[13px] font-semibold px-2 py-1 rounded-full shrink-0" style={{color: trackColor, background: barColor}}>
                             {stepText}
                         </span>
