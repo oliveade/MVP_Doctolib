0db5e604965b8918152f4706f060b8d115151588
oliveade <olive.ade@eemi.com>
2025-09-25 14:42:32 +0200

    recherche page

--- compagnon-sante/src/components/search/InfoBanner.tsx
+++ compagnon-sante/src/components/search/InfoBanner.tsx
@@ -3,7 +3,7 @@ export default function InfoBanner() {
   return (
     <div className="rounded-2xl bg-[#D8ECFA] p-3 text-[14px] text-slate-700 flex gap-3">
       <div className="h-9 w-9 rounded-xl bg-[#EAF5FE] flex items-center justify-center">
-        <img src="/avatar/care.png" alt="" className="h-5 w-5" />
+        <img src="/avatar/care.png" alt="" className="h-7 w-7" />
       </div>
       <p>
         Pour faciliter votre parcours de soins, il est recommandé de
